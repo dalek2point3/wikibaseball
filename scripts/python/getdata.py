ffcb57af67746a6c9df52d5b71eb85ba9c736971
Abhishek <dalek2point3@gmail.com>
2014-07-24 22:04:04 -0400

    framework for getdata script

---
+++ scripts/python/getdata.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+""" This file downloads all relevant data for this project """
+
+__author__      = "dalek2point3"
+__copyright__   = "MIT License"
+
+def get_lahman():
+    pass
+
+def get_playerrevs():
+    pass
+
+def get_traffic():
+    pass
+
+def main():
+    pass
+
+f __name__ == "__main__":
+    main()
+
+
